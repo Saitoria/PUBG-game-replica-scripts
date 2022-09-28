b48c4c59fe4c5322f35b8cbdc1c155d1294eb305
Saitoria <saronisaitoria@gmail.com>
2022-09-28 12:16:23 +0300

    ChatMessageHandler

---
+++ ChatMessageHandler.cs
@@ -0,0 +1,58 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatMessageHandler : MonoBehaviourPun
+{
+
+    public Text messageReceived;
+
+    enum EventCodes {
+        chatmessage = 0,
+              playAudio = 1
+
+    }
+    private void OnEnable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+    }
+    private void OnDisable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived -= OnEvent;
+
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        byte eventCode = photonEvent.Code;
+        object content = photonEvent.CustomData;
+        EventCodes code = (EventCodes)eventCode;
+
+        if (code == EventCodes.chatmessage)
+        {
+            object[] datas = content as object[];
+                
+            messageReceived.text = (string)datas[0];
+        }
+    }
+
+    public void SendMsg(string message)
+    {
+        object[] datas = new object[] { message };
+        RaiseEventOptions options = new RaiseEventOptions
+        {
+            CachingOption = EventCaching.DoNotCache,
+            Receivers = ReceiverGroup.All,
+            
+            
+        };
+        SendOptions sendOptions = new SendOptions();
+        sendOptions.Reliability = true;
+
+        PhotonNetwork.RaiseEvent((byte)EventCodes.chatmessage, datas, options, sendOptions);
+    }
+}
