9cd500d3525e1dd7df541996b768aa042685035a
Saitoria <saronisaitoria@gmail.com>
2022-09-28 12:08:28 +0300

    ChatButton

---
+++ ChatButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatButton : MonoBehaviour
+{
+    public GameObject chatMessages;
+
+    public void OnClick()
+    {
+        if (chatMessages.activeSelf)
+        {
+            chatMessages.SetActive(false);
+        }
+        else {
+            chatMessages.SetActive(true);
+        }
+    }
+}
