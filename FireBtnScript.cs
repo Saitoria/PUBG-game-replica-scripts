6a3899b70252b490ad873b313e052aa35d2bae69
Saitoria <saronisaitoria@gmail.com>
2022-09-28 12:19:29 +0300

    FireBtnScript

---
+++ FireBtnScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class FireBtnScript : MonoBehaviour, IPointerDownHandler,IPointerUpHandler
+{
+    public MyPlayer player;
+    bool firing = false;
+    public void SetPlayer(MyPlayer _player)
+    {
+        player = _player;
+    }
+
+    public void Update()
+    {
+        if (firing)
+        {
+            player.Fire();
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        firing = true;
+        player.Fire();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        firing = false;
+        player.FireUp();
+    }
+}
